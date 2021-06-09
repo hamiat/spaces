e74fc727ffcda1fbe31dc579ce4bbe3413c00115
hamiat <hamiat.n@hotmail.com>
2021-06-09 10:57:50 +0200

    link to next game

--- tolevelthree.js
+++ tolevelthree.js
@@ -3,9 +3,9 @@ window.onload = init();
 function init() {
   const toLevelThree = document.querySelector("#door-to-level-three");
 
-  toLevelThree.addEventListener("click", nextLevel);
+  toLevelThree.addEventListener("click", nextGame);
 
-  function nextLevel() {
+  function nextGame() {
     
     location.href = "https://sumanasattar.github.io/A-frame-project";
   }
